8f65ccbc57ba64b180bd48349f813310f6e3c3ec
Madeleine Negård <madelne@stud.ntnu.no>
2025-06-30 14:27:12 +0200

    Base colors

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,8 +1,25 @@
-import type { Config } from 'tailwindcss';
-import daisyui from 'daisyui';
+// tailwind.config.ts
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  plugins: [daisyui],
-};
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,vue}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'navy': '#040913',
+        'dark-blue': '#00253c',
+        'blue-100': '#00253c',
+        'blue-300': '#040913',
+        'neutral-100': '#C5CDD6',
+        'neutral-200': '#F1F3F4',
+        'neutral-300': '#A8B2BC',
+      }
+    },
+  },
+  plugins: [],
+}
 
-export default config;
+export default config
