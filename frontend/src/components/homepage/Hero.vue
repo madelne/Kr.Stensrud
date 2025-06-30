7c1b365f510685884ecf079483be5e80c87fd495
Madeleine Negård <madelne@stud.ntnu.no>
2025-06-30 17:10:06 +0200

    Hero

---
+++ frontend/src/components/homepage/Hero.vue
@@ -0,0 +1,13 @@
+<template>
+<div class="hero pt-20 pb-80">
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">Stolte tradisjoner</h1>
+      <p class="py-6">
+        Spesialister på ombygging og rehabilitering
+      </p>
+      <button class="contact-btn">Kontakt oss</button>
+    </div>
+  </div>
+</div>
+</template>
