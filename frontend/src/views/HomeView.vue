98d53a1754a60df1a61be39b4ad1dc51df94cdc6
Madeleine Negård <madelne@stud.ntnu.no>
2026-01-02 14:21:47 +0100

    Remove global values

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,41 +2,10 @@
 import Hero from '@/components/homepage/Hero.vue';
 import Navbar from '@/components/Navbar.vue';
 import MainCards from '@/components/homepage/MainCards.vue';
-import { onMounted, onUnmounted, ref } from 'vue';
 import Footer from '@/components/Footer.vue';
-
-const showScrollTop = ref(false);
-
-const scrollToTop = () => {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth'
-  });
-};
-
-const handleScroll = () => {
- showScrollTop.value = window.scrollY > 300
-}
-
-onMounted(() => {
- window.addEventListener('scroll', handleScroll)
-})
-
-onUnmounted(() => {
- window.removeEventListener('scroll', handleScroll)
-})
 </script>
 
 <template>
-  <Navbar id="top"/>
   <Hero />
   <MainCards />
-  <Footer />
-  <button
-    v-if="showScrollTop"
-    @click="scrollToTop"
-    class="fixed bottom-6 right-6 bg-neutral-300 hover:bg-neutral-100 text-blue-300 py-3 px-4.5 rounded-full shadow-lg transition-all duration-300 z-50"
-  >
-    ↑
-  </button>
 </template>
