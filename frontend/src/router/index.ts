c008d2c44a767aad00e785082defd5b54e23d511
Madeleine Negård <madelne@stud.ntnu.no>
2026-01-02 23:48:22 +0100

    Route to project id

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProjectsView from '@/views/ProjectsView.vue'
+import SingleProjectView from '@/views/SingleProjectView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/Prosjekter',
       name: 'projects',
       component: ProjectsView,
+    },
+    {
+      path: '/Prosjekter/:_id',
+      name: 'project',
+      component: SingleProjectView,
     },
   ],
 })
