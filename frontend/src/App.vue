79ee43ee55b63e95f6b18ac30e00ffbd8778a0a3
Madeleine Negård <madelne@stud.ntnu.no>
2026-02-12 11:51:15 +0100

    Add router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
-import { onMounted, onUnmounted, ref } from 'vue';
+import { onMounted, onUnmounted, ref, computed } from 'vue';
+import { useRoute } from 'vue-router';
 import Footer from '@/components/Footer.vue';
 import Navbar from '@/components/Navbar.vue';
+
+const route = useRoute();
 const showScrollTop = ref(false);
+
+const routeKey = computed(() => route.path);
 
 const scrollToTop = () => {
   window.scrollTo({
@@ -26,17 +31,18 @@ onUnmounted(() => {
 <template>
   <div id="App">
     <Navbar id="top"/>
-    <router-view />
+    <div class="min-h-screen p-3">
+      <router-view :key="routeKey" />
 
-    <!-- Scroll to top button -->
-    <button
-      v-if="showScrollTop"
-      @click="scrollToTop"
-      class="fixed bottom-6 right-6 bg-neutral-300 hover:bg-neutral-100 text-blue-300 py-3 px-4.5 rounded-full shadow-lg transition-all duration-300 z-50"
-    >
-      ↑
-    </button>
-
+      <!-- Scroll to top button -->
+      <button
+        v-if="showScrollTop"
+        @click="scrollToTop"
+        class="fixed bottom-6 right-6 bg-neutral-300 hover:bg-neutral-100 text-blue-300 py-3 px-4.5 rounded-full shadow-lg transition-all duration-300 z-50"
+      >
+        ↑
+      </button>
+    </div>
     <Footer/>
   </div>
 </template>
