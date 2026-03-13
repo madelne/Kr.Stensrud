86dbc4e691296ab6f3a7a379f1cd3b936bc4e3c8
Madeleine Negård <madelne@stud.ntnu.no>
2026-03-13 14:35:40 +0100

    Add page to schematypes

--- frontend/sanity/schemaTypes/index.ts
+++ frontend/sanity/schemaTypes/index.ts
@@ -1,9 +1,10 @@
 import blockContent from './blockContent'
 import category from './category'
 import project from './project'
+import page from './link'
 import author from './author'
 import service from './service'
 import textfield from './textfield'
 import textfieldType from './textfieldType'
 
-export const schemaTypes = [project, author, category, service, textfield, textfieldType, blockContent]
+export const schemaTypes = [project, author, category, service, textfield, textfieldType, blockContent, page]
