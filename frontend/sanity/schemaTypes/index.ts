9592946ad63a24ca2c39a7bf9b4ab74270bcc402
Madeleine Negård <madelne@stud.ntnu.no>
2026-03-29 15:37:36 +0200

    Add link type

--- frontend/sanity/schemaTypes/index.ts
+++ frontend/sanity/schemaTypes/index.ts
@@ -1,10 +1,10 @@
 import blockContent from './blockContent'
-import category from './category'
+import category from './projectCategory'
 import project from './project'
-import page from './link'
+import link from './link'
 import author from './author'
 import service from './service'
 import textfield from './textfield'
 import textfieldType from './textfieldType'
 
-export const schemaTypes = [project, author, category, service, textfield, textfieldType, blockContent, page]
+export const schemaTypes = [link, project, author, category, service, textfield, textfieldType, blockContent]
