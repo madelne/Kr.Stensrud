ca7316a822fa94ac5f79dbc6352a387f01bd19fe
Madeleine Negård <madelne@stud.ntnu.no>
2026-03-29 15:39:10 +0200

    Rename field

--- frontend/sanity/schemaTypes/textfieldType.ts
+++ frontend/sanity/schemaTypes/textfieldType.ts
@@ -1,19 +1,20 @@
 import {defineField, defineType} from 'sanity'
 
+// Textfield type, which is a reference in the textfield post. Type includes the type's name, such as "about us - intro", etc.
 export default defineType({
   name: 'textfieldType',
   title: 'Teksttype',
   type: 'document',
   fields: [
     defineField({
-      name: 'type',
-      title: 'Type',
+      name: 'textfieldtype',
+      title: 'Textfield type',
       type: 'string',
     }),
   ],
   preview: {
     select: {
-      title: 'type',
+      title: 'textfieldtype',
     },
   },
 })
