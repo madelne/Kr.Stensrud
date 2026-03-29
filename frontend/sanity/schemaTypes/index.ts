d18bc8537310e54a21181b6a41cdd7cd0fb941c6
Madeleine Negård <madelne@stud.ntnu.no>
2026-03-29 19:10:06 +0200

    Import schema types

--- frontend/sanity/schemaTypes/index.ts
+++ frontend/sanity/schemaTypes/index.ts
@@ -6,5 +6,8 @@ import author from './author'
 import service from './service'
 import textfield from './textfield'
 import textfieldType from './textfieldType'
+import image from './heroImage'
+import hmsImage from './hmsImage'
+import certification from './certification'
 
-export const schemaTypes = [link, project, author, category, service, textfield, textfieldType, blockContent]
+export const schemaTypes = [link, project, author, category, service, textfield, textfieldType, blockContent, image, hmsImage, certification]
